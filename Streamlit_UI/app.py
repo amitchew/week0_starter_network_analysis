48f4267b713544f38ab8dc79d7c0ed40336db00d
amitchew <49229715+amitchew@users.noreply.github.com>
2023-12-01 23:28:44 +0300

    feat: stream lit file upload nad graph
    feat: stream lit file upload nad graph
    
    Signed-off-by: amitchew <49229715+amitchew@users.noreply.github.com>

--- Streamlit_UI/app.py
+++ Streamlit_UI/app.py
@@ -1,33 +1,74 @@
 import streamlit as st
 import pandas as pd
+import json
 import matplotlib.pyplot as plt
 
-st.title('Slack Messages')
-st.text('Channel Members Analysis')
+def upload_slack_file():
+    uploaded_file = st.file_uploader("Upload your Slack message analysis file (JSON)", type=["json"])
 
-upload_file = st.file_uploader("Upload your file")
+    if uploaded_file is not None:
+        st.success("File uploaded successfully!")
 
-if upload_file:
-    df = pd.read_csv(upload_file)
-    st.header('Data Header')
-    st.write(df.head())
+        # Read the uploaded JSON file
+        content = uploaded_file.read()
+        data = json.loads(content)
 
-    st.header('Data Statistics')    
-    st.write(df.describe())  
+        # Convert JSON data to DataFrame
+        df = pd.json_normalize(data)
 
-    df = df[:3]
-    fig, ax = plt.subplots(1, 1)
-    ax.bar(df['name'], df['members'])
-    ax.set_xlabel('Name')
-    ax.set_ylabel('Members')
+        # Display the DataFrame
+        st.write("Displaying the content of the uploaded file:")
+        st.write(df)
 
-    st.pyplot(fig)
+        # Plotting options
+        st.subheader("Plotting Options:")
+        plot_option = st.selectbox("Select Plotting Option", ["Top Users by Reply Count", "Reply Counts per User per Channel"])
 
+        if plot_option == "Top Users by Reply Count":
+            plot_top_users_by_reply_count(df)
+        elif plot_option == "Reply Counts per User per Channel":
+            plot_reply_counts_per_user_per_channel(df)
 
+def plot_top_users_by_reply_count(df):
+    user_reply_counts = df.groupby('sender_name')['reply_count'].sum()
+    sorted_user_reply_counts = user_reply_counts.sort_values(ascending=False)
 
-    # fig, ax = plt.subplots(1,1)
-    # ax.scatter(x=df['name'], y=df['members'])  
-    # ax.set_xlabel('Name')
-    # ax.set_ylabel('Members')
+    # Plot the top users by reply count
+    plt.figure(figsize=(15, 7.5))
+    sorted_user_reply_counts.plot(kind='bar')
+    
+    plt.title('Top Users by Reply Counts')
+    plt.xlabel('User')
+    plt.ylabel('Number of Replies')
+    st.pyplot()
 
-    # st.pyplot(fig)
+def plot_reply_counts_per_user_per_channel(df):
+    user_channel_reply_counts = df.groupby(['channel', 'sender_name'])['reply_count'].sum().unstack()
+
+    # Plot the reply counts per user per channel
+    plt.figure(figsize=(15, 7.5))
+    user_channel_reply_counts.plot(kind='bar', stacked=True)
+    
+    plt.title('Reply Counts of User/Channel')
+    plt.xlabel('Channel')
+    plt.ylabel('Number of Replies')
+    plt.legend(title='Sender Name', bbox_to_anchor=(1, 1))
+    st.pyplot()
+
+def main():
+    st.title("Slack Message Analysis App")
+    st.sidebar.header("Navigation")
+
+    app_mode = st.sidebar.selectbox("Choose the app mode", ["Upload File", "About"])
+
+    if app_mode == "Upload File":
+        st.header("Upload Slack Message Analysis File")
+        upload_slack_file()
+    elif app_mode == "About":
+        st.header("About")
+        st.write(
+            "This Streamlit app allows you to upload a Slack message analysis file in JSON format and displays its contents."
+        )
+
+if __name__ == "__main__":
+    main()
